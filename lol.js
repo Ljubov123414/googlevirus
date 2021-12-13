880ee4214cc6100e349ba12459813c8d7c7c4e0c
Human <86323083+Ljubov123414@users.noreply.github.com>
2021-12-13 10:55:32 -0500

    UPD 0.7

--- lol.js
+++ lol.js
@@ -1,1 +1,5 @@
-window.open("https://bonzi.link", "_blank")
+$('a.yourlink').click(function(e) {
+    e.preventDefault();
+    window.open('http://yoururl1.com');
+    window.open('http://yoururl2.com');
+});
